2092894dcfdf0de8502604b20209a19d7af8c867
MartinClementson <martin.clementson@outlook.com>
2016-11-14 12:56:52 +0100

    Resource Class is implemented.

--- ResourceLib/ResourceLib/Resource.cpp
+++ ResourceLib/ResourceLib/Resource.cpp
@@ -2,11 +2,51 @@
 
 
 
-Resource::Resource()
+Resources::Resource::Resource()
 {
 }
 
 
-Resource::~Resource()
+Resources::Resource::~Resource()
 {
 }
+
+const bool Resources::Resource::IsType(ResourceType type) const
+{
+	return m_resourceData.m_resType == type ? true : false;
+}
+
+const bool Resources::Resource::SetName(std::string name) 
+{
+	try {
+
+		memcpy(m_resourceData.m_name, name.c_str, size_t(256));
+	}
+	catch (...)
+	{
+		return false;
+	}
+	return true;
+}
+
+const std::string Resources::Resource::GetName() const
+{
+	return std::string(m_resourceData.m_name);
+}
+
+const unsigned int Resources::Resource::GetId() const
+{
+	return m_resourceData.m_id;
+}
+
+char * Resources::Resource::GetDataAsBinary(size_t* size, bool* result)
+{
+	*size = sizeof(RawResourceData);
+	
+	if (result != nullptr)
+		*result = true;
+
+	return (char*)&m_resourceData;
+
+};
+
--- ResourceLib/ResourceLib/Resource.h
+++ ResourceLib/ResourceLib/Resource.h
@@ -1,17 +1,57 @@
 #ifndef RESOURCELIB_RESOURCE_RESOURCE_H
 #define RESOURCELIB_RESOURCE_RESOURCE_H
 #include "DefineHeader.h"
+#include <unordered_map>
 
-class Resource
+
+namespace Resources
 {
-public:
-	Resource();
-	virtual ~Resource();
 
+	enum ResourceType
+	{
+		RES_UNKOWN  = - 1,
+		RES_TEXTURE = 0,
+		RES_MATERIAL,
+		RES_MESH,
+		RES_SKELETON,
+		RES_SOUND,
+		RES_LIGHT,
+		RES_ANIMATION,
+		RES_UI
+	};
 
-};
+	class Resource
+	{
+		struct RawResourceData
+		{
+			char m_name[256]; //change this variable in UML (when it works again) then delete this comment
+			unsigned int m_id;
+			ResourceType m_resType;
+			// remove function in UML
+		};
+	
+	protected:
+		std::unordered_map<std::string, Resource*> m_connections;
+		RawResourceData m_resourceData;
 
+	public:
+		Resource();
+		virtual ~Resource();
 
+		const bool IsType(ResourceType type) const;
+		const ResourceType GetResourceType() const { return m_resourceData.m_resType; };
+
+		const bool SetName(std::string name) ;
+		const std::string GetName() const;
+
+		const unsigned int GetId() const;
+		
+	
+		virtual char * GetDataAsBinary(size_t* size, bool* result = nullptr) = 0;
+	};
+	
+
+}
 
 #endif
 
