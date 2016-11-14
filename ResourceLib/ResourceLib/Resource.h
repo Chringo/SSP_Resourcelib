f2d3b8ec9a8120c31cfcc366d97233ef69ddfcb9
MartinClementson <martin.clementson@outlook.com>
2016-11-14 10:08:13 +0100

    Added Resource class and filters

---
+++ ResourceLib/ResourceLib/Resource.h
@@ -0,0 +1,17 @@
+#ifndef RESOURCELIB_RESOURCE_RESOURCE_H
+#define RESOURCELIB_RESOURCE_RESOURCE_H
+#include "DefineHeader.h"
+
+class Resource
+{
+public:
+	Resource();
+	virtual ~Resource();
+
+
+};
+
+
+
+#endif
+
